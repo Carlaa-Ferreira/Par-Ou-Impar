19429dbfb68a9638fcdc399d0647d026d91e5112
Carla <ana_carla_0202@yahoo.com.br>
2022-06-26 18:11:32 -0300

    ParOuImpar

---
+++ Aula29a/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula29a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("ÍMPAR");
+            }
+
+        }
+    }
+}
